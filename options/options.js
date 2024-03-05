b2ef34ea9dc20e4a2ea06e8fa0aaa5a93341239f
Crowdbotics <team@crowdbotics.com>
2024-03-05 23:34:49 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "datingprd-46828",
+  url: "https://datingprd-46828.botics.co",
+  api: "https://datingprd-46828.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
